90c847ba58caad6e394758098623aad3f315456d
Aditya Sharma <subhaiyash@gmail.com>
2025-11-30 22:13:04 +0530

    cors policy updated

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ app.use(cors(
             "http://localhost:5173",
             "http://localhost:5174",
             "http://localhost:3000",
+            "https://craxy-notes-frontend.onrender.com",
         ],
         credentials: true,
     }
